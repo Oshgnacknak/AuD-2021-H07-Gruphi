32c23e115c37df83be356be8df26d195f880d744
Oshgnacknak <osh@oshgnacknak.de>
2021-09-05 22:46:07 +0200

    Have default algorithms in GruphiMain

--- src/main/java/de/oshgnacknak/gruphi/GruphiMain.java
+++ src/main/java/de/oshgnacknak/gruphi/GruphiMain.java
@@ -1,11 +1,16 @@
 package de.oshgnacknak.gruphi;
 
+import h07.algorithm.Dijkstra;
+import h07.algorithm.Kruskal;
+import h07.graph.DirectedGraphFactory;
+
 public class GruphiMain {
 
     public static void main(String[] args) {
         var gruphi = new GruphiBuilder()
-            // .setDirectedGraphFactory(someFactory)
-            // .setShortestPathsAlgorithm(somePathFinder)
+            .setDirectedGraphFactory(DirectedGraphFactory.defaultFactory())
+            .setShortestPathsAlgorithm(new Dijkstra<>())
+            .setMinimumSpanningForestAlgorithm(new Kruskal<>())
             .createGruphi();
 
         var frame = new GruphiFrame(gruphi);
